55589086933ee5a0b323997baa1122c6b5a4b7d0
Khalilur Rahman <khal.rahman@gmail.com>
2015-04-25 22:14:40 +0800

    Created dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,9 @@
 class PagesController < ApplicationController
+  before_action :authenticate_user!, only: [:dashboard]
   def home
+  end
+
+  def dashboard
+  	@books = current_user.books
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   resources :books
   devise_for :users
   root 'pages#home'
+
+  get 'dashboard' => "pages#dashboard"
 end
